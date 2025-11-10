ecc608ab09fa4557e9ad58c27504f715b9efb24a
James LAU <james.lau.he@gmail.com>
2025-11-10 09:47:52 +0800

    routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css'
+	import favicon from '$lib/assets/favicon.svg'
 
-	let { children } = $props();
+	let { children } = $props()
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/posts">Posts</a>
+		</li>
+	</ul>
+</nav>
 
 {@render children()}
---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,19 @@
+<div class="post">
+    <aside>
+        <h4>Posts</h4>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="/posts/one">One</a>
+                </li>
+                <li>
+                    <a href="/posts/two">Two</a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+    <main>
+        <slot />
+    </main>
+</div>
